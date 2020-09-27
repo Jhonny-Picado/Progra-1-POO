91de25bb7b6bbf8f9928d5eec4ad602623f3263f
Christoph <crvindas95@gmail.com>
2020-09-27 17:06:26 -0600

    Java armadura

---
+++ TiendaRPG/src/tiendarpg/Armadura.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendarpg;
+
+/**
+ *
+ * @author cva
+ */
+public class Armadura extends Item{
+    
+    //Atributos propios de la clase arma 
+    
+    private int tamaño;
+    private String material;
+    //Constructor que modifica los atributos de Arma
+    public Armadura(String nombre, String color, int precio, int nivel, int poder, String tipo,int tamaño, String material){
+        
+        
+        
+        super(nombre,color,precio,nivel,poder);
+        this.tamaño= tamaño;
+        this.material=material;
+    }
+    
+    //toString, muestra el contenido de los atributos de Arma
+    @Override
+    public String toString  (){ //Getter
+        return super.getDatos()+" tamaño: "+ tamaño+ ",  material: "+material;
+    }
+}
