f7671e6fa645421f9b5c8e73ca70c44ef8f91e6b
Jhonny Picado Vega <jhonnypv14@estudiantec.cr>
2020-09-28 17:07:37 -0600

    Se añadieron las librerias Okhttp, Json simple y okio. Además, se hizo la conexión con el API.

--- TiendaRPG/src/tiendarpg/Controlador.java
+++ TiendaRPG/src/tiendarpg/Controlador.java
@@ -4,7 +4,12 @@
  * and open the template in the editor.
  */
 package tiendarpg;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import java.io.IOException;
 import java.util.*;
+import org.json.simple.JSONObject;
 /**
  * @author Christopher Vindas
  * @author Jhonny Picado Vega
@@ -13,7 +18,9 @@ public class Controlador {
     
     public static List<Item> items = new ArrayList<>();
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        
         String [] vColor={"Azul", "Rosa", "Morado", "Purpura", "Cafe", "Negro", "Celeste", "Terracota", "Negro","Negro","Negro"};
         String [] vNombre={"Maria", "Juan", "Saul", "Isaac", "Manuel", "Lucas", "Lewandowski", "Capdevila", "Hola", "Hola", "Hola"};
         int [] vPrecio={12, 5, 7, 9, 2, 2, 45, 90, 12, 12, 12};
@@ -35,5 +42,25 @@ public class Controlador {
         for(Item n:items) {
             System.out.println(n.toString());
         }
+        //Parte del API
+        
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+	.url("https://magic-aliexpress1.p.rapidapi.com/api/category/200001081/products?targetCurrency=EUR&fastDeliveryDays=3&shipToCountry=FR&maxSalePrice=10&lg=fr")
+	.get()
+	.addHeader("x-rapidapi-host", "magic-aliexpress1.p.rapidapi.com")
+	.addHeader("x-rapidapi-key", "8ac4177326mshd5244b602b18176p145d17jsn505c099c181c")
+	.build();
+
+        Response response = client.newCall(request).execute();
+        //System.out.println(response.body().string());
+        
+        JSONObject object = new JSONObject();
+        object.put("Hola",response.body().string());
+        System.out.println("\n"+"\n"+object.values());
+        
+        
+        
     }
 }
