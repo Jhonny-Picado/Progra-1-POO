71e4db65476720297006eaa699c78555452f258c
Jhonny Picado Vega <jhonnypv14@estudiantec.cr>
2020-10-06 21:02:13 -0600

    Se cambio el la libreria del API, agregue tienda

--- TiendaRPG/src/tiendarpg/Controlador.java
+++ TiendaRPG/src/tiendarpg/Controlador.java
@@ -6,64 +6,51 @@
 package tiendarpg;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
 import java.io.IOException;
-import java.util.*;
-import org.json.simple.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * @author Christopher Vindas
  * @author Jhonny Picado Vega
  */
 public class Controlador {
     
-    public static List<Item> items = new ArrayList<>();
+
     
-    public static void main(String[] args) throws IOException {
-        
-        
-        String [] vColor={"Azul", "Rosa", "Morado", "Purpura", "Cafe", "Negro", "Celeste", "Terracota", "Negro","Negro","Negro"};
-        String [] vNombre={"Maria", "Juan", "Saul", "Isaac", "Manuel", "Lucas", "Lewandowski", "Capdevila", "Hola", "Hola", "Hola"};
-        int [] vPrecio={12, 5, 7, 9, 2, 2, 45, 90, 12, 12, 12};
-        int    [] vNivel={1,2,3,4,5,6,7,8, 10,10,10};
-        int    [] vPoder={14,15,13,12,12,234,54,56,50,50,50};
-        String [] vTipo ={"Cuchillo", "Pistola", "Pistola", "Flechas", "Pipolin", "Fuego", "Agua", "Tierra", "Bebida", "Comida", "Bebida"};
-        int [] vPeso={12,1,11,17,12,2,4,6};
-        
-        for (int i=0; i<8; i++){
-            Item arma = new Arma(vNombre[i], vColor[i], vPrecio[i], vNivel[i], vPoder[i], vTipo[i], vPeso[i]);
-            items.add(arma);
-        }
-        
-        for (int i=8; i<11; i++){
-            Item arma = new Consumible (vNombre[i], vColor[i], vPrecio[i], vNivel[i], vPoder[i], vTipo[i]);
-            items.add(arma);
-        }
-        
-        for(Item n:items) {
-            System.out.println(n.toString());
-        }
-        //Parte del API
+    public static void main(String[] args) throws IOException{
+        Tienda prueba = null;
         
         OkHttpClient client = new OkHttpClient();
-    
+   
         Request request = new Request.Builder()
-	.url("https://walmart.p.rapidapi.com/products/get-details?usItemId=55885487")
+	.url("https://walmart.p.rapidapi.com/departments/list")
 	.get()
 	.addHeader("x-rapidapi-host", "walmart.p.rapidapi.com")
 	.addHeader("x-rapidapi-key", "8ac4177326mshd5244b602b18176p145d17jsn505c099c181c")
 	.build();
 
-        Response response = client.newCall(request).execute();
-        //System.out.println(response.body().string());
+        ResponseBody responseBody = client.newCall(request).execute().body();        
+        //System.out.println(responseBody.string());
         
-        JSONObject object = new JSONObject();
-        object.put("Hola",response.body().string());
-        System.out.println("\n"+"\n"+object.values());
+        JSONObject json=new JSONObject(responseBody.string());
+
+        /*
+        JSONArray myArray = new JSONArray();
+        myArray.add(json.get("departments"));
+        JSONArray myArray2 = new JSONArray();
+        myArray2.add(myArray.get(0));
+        System.out.println(myArray);
+        System.out.println(json);
+        */
+        JSONArray arr = json.getJSONArray("departments");
         
-        
-        System.out.println("\n"+"\n"+object.get("data"));//Acá debería agarrar los contenidos de la etiqueta data del JSON, pero no lo hace, más o menos va por ahí 
-        
-        
+            String post_id = arr.getJSONObject(4).getString("name");
+            //prueba.algo(post_id);
+           // prueba.retorno();
+            System.out.println(post_id);
+
         
     }
 }
---
+++ TiendaRPG/src/tiendarpg/Tienda.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendarpg;
+
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tienda {
+    
+    private final String nombre;
+    private int dinero;
+    private List<Item> inventarioTienda;
+    private  int j;
+    private String casa;
+    public Tienda(List<Item> items) {
+        nombre ="Sogeking Store";
+        dinero=10000000;
+        inventarioTienda=items;
+    }
+    
+    public void Eliminaritem(int j){
+        this.j=j;
+        dinero+=inventarioTienda.get(j).precio;
+        inventarioTienda.remove(j);
+        
+    }
+
+    public String dimeInventario(){
+        System.out.println("El nombre de la tienda es:" + nombre+ "Dinero: "+dinero);
+        int cont = 0;
+        for (Item i : inventarioTienda){
+            System.out.println("El item de la tienda # "+cont+"es: "+i);
+            cont++;
+        }
+       return "El nombre de la tienda es:" + nombre+ "Dinero: "+dinero;
+    }
+    
+    public void algo(String yuen){
+        
+        this.casa=yuen;
+    }
+    
+    public String retorno(){
+    
+        return casa; 
+}
+
+    
+}
